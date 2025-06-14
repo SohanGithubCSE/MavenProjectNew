11545f170cf5152ce99ec4ff3dbab45b50d3fbfe
hp <hp@DESKTOP-P0RJMCF>
2025-06-14 14:35:20 +0530

    Commit Operation No: 1

---
+++ Log4J2ExampleProject2/src/main/java/Log4J2Example.java
@@ -0,0 +1,14 @@
+import org.apache.logging.log4j.LogManager; // class
+import org.apache.logging.log4j.Logger; // interface
+
+public class Log4J2Example
+{
+    private static final Logger l = LogManager.getLogger(Log4J2Example.class);
+
+    public static void main(String[] args) 
+    {
+        l.info("This is an info message");
+        l.warn("This is a warning message");
+        l.error("This is an error message");
+    }
+}
