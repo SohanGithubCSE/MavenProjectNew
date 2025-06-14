23be8c4dcba4d5fb026ce1758964c4faeb9702a4
hp <hp@DESKTOP-P0RJMCF>
2025-06-14 14:47:16 +0530

    Commit Operation No : 2

--- Log4J2ExampleProject2/src/main/java/Log4J2Example.java
+++ Log4J2ExampleProject2/src/main/java/Log4J2Example.java
@@ -8,6 +8,7 @@ public class Log4J2Example
     public static void main(String[] args) 
     {
         l.info("This is an info message");
+        l.info("This is another info");
         l.warn("This is a warning message");
         l.error("This is an error message");
     }
